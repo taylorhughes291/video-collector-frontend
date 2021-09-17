8792a18ab87a9584e7bacdec07f866c69c63377f
Taylor Hughes <taylor hughes>
2021-09-16 23:46:12 -0700

    brain fried, saving work

--- src/pages/Shuffle.js
+++ src/pages/Shuffle.js
@@ -6,22 +6,30 @@ const Shuffle = (props) => {
     const [shows, setShows] = useState([])
     const [selectedShows, setSelectedShows] = useState([])
 
-    const selectEpisode = () => {
-        let availableEpisodes
+    const selectEpisode = (showsArray) => {
+        let availableEpisodes = props.episodes.filter((item, index) => {
+            return (
+                showsArray.includes(item.episodeType)
+            )
+        })
         if (props.user !== "") {
-            availableEpisodes = props.episodes.filter((item, index) => {
+            availableEpisodes = availableEpisodes.filter((item, index) => {
                 return (
                     !props.episodesViewed.includes(item._id)
                 )
             })
-        } else {
+        }
+        if (availableEpisodes.length < 1) {
+            alert("There are no more remaining videos to select. Please select another category. In the meantime, we will shuffle all videos.")
             availableEpisodes = props.episodes
         }
+
         let availableShows = props.episodes.map((item, index) => {
             return (
                 item.episodeType
             )
         })
+
         availableShows = [...new Set(availableShows)]
         return {
             episode: availableEpisodes[Math.floor(Math.random() * availableEpisodes.length)],
@@ -35,8 +43,10 @@ const Shuffle = (props) => {
         const index = newArray.indexOf(show)
         index >= 0 ? newArray.splice(index, 1) : newArray.push(show)
         setSelectedShows(newArray)
+        const shuffleData = selectEpisode(newArray)
+        props.setSelectedEpisode(shuffleData.episode)
     }
-    console.log("state: ", selectedShows);
+    
     const ShowFender = () => {
         const showsRender = shows.map((item, index) => {
             return (
@@ -56,7 +66,7 @@ const Shuffle = (props) => {
     
     useEffect(() => {
         if (props.episodes.length > 1) {
-            const shuffleData = selectEpisode()
+            const shuffleData = selectEpisode(shows)
             props.setSelectedEpisode(shuffleData.episode)
             setShows(shuffleData.shows)
         }
@@ -73,6 +83,12 @@ const Shuffle = (props) => {
                 <h3>This is the Shuffle Page</h3>
                 <div>
                     <h3>What shows do you want to watch?</h3>
+                    <button
+                        onClick={() => setSelectedShows([])}
+                    >De-Select All</button>
+                    <button
+                        onClick={() => setSelectedShows(shows.slice(0))}
+                    >Select All</button>
                     <form>
                         <ShowFender />
                     </form>
