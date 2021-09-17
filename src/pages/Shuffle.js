9927360b75890c7b791a58d93dc29b3af9ebb57b
Taylor Hughes <taylor hughes>
2021-09-16 23:26:29 -0700

    reminded the hard way that you can't just copy an array, you need to go out of your way to actually slice it. Wow I just wasted so much time on this.

--- src/pages/Shuffle.js
+++ src/pages/Shuffle.js
@@ -4,6 +4,7 @@ import Episode from "./Episode"
 const Shuffle = (props) => {
 
     const [shows, setShows] = useState([])
+    const [selectedShows, setSelectedShows] = useState([])
 
     const selectEpisode = () => {
         let availableEpisodes
@@ -16,18 +17,55 @@ const Shuffle = (props) => {
         } else {
             availableEpisodes = props.episodes
         }
-        console.log(props.episodes, availableEpisodes);
-        return availableEpisodes[Math.floor(Math.random() * availableEpisodes.length)]
+        let availableShows = props.episodes.map((item, index) => {
+            return (
+                item.episodeType
+            )
+        })
+        availableShows = [...new Set(availableShows)]
+        return {
+            episode: availableEpisodes[Math.floor(Math.random() * availableEpisodes.length)],
+            shows: availableShows
+        }
         
     }
 
+    const handleCheck = (show) => {
+        const newArray = selectedShows.slice(0)
+        const index = newArray.indexOf(show)
+        index >= 0 ? newArray.splice(index, 1) : newArray.push(show)
+        setSelectedShows(newArray)
+    }
+    console.log("state: ", selectedShows);
+    const ShowFender = () => {
+        const showsRender = shows.map((item, index) => {
+            return (
+                <div key={index}>
+                    <input 
+                        type="checkbox" 
+                        defaultChecked={selectedShows.includes(item) ? true : false }
+                        value={item} 
+                        id={item} 
+                        onChange={() => handleCheck(item)} />
+                    <label htmlFor={item}>{item}</label>
+                </div>
+            )
+        })
+        return showsRender
+    }
     
     useEffect(() => {
         if (props.episodes.length > 1) {
-            props.setSelectedEpisode(selectEpisode())
-            console.log(props.selectedEpisode);
+            const shuffleData = selectEpisode()
+            props.setSelectedEpisode(shuffleData.episode)
+            setShows(shuffleData.shows)
         }
     }, [props.episodes, props.episodesViewed])
+
+    useEffect(() => {
+        const allShows = shows.slice(0)
+        setSelectedShows(allShows)
+    }, [shows])
 
     const loaded = () => {
         return (
@@ -35,7 +73,9 @@ const Shuffle = (props) => {
                 <h3>This is the Shuffle Page</h3>
                 <div>
                     <h3>What shows do you want to watch?</h3>
-    
+                    <form>
+                        <ShowFender />
+                    </form>
                 </div>
                 <Episode 
                     selectedEpisode={props.selectedEpisode}
